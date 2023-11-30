eb57e5b0992171c2e3ea139e5236ba074c23a552
MRJD <M.Dagleish@campus.lmu.de>
2023-11-30 21:09:33 +0100

    Add print_prog_header function and ExecTimes struct

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,27 @@ mod mol_int_and_deriv;
 mod molecule;
 mod print_utils;
 
+use crate::print_utils::print_prog_header;
 use molecule::Molecule;
 
 fn main() {
+    //##################################
+    //###           HEADER           ###
+    //##################################
+    let mut exec_times = print_utils::ExecTimes::new();
+    exec_times.start("total");
+
+    print_prog_header();
+
+    exec_times.start("molecule");
     let mol = Molecule::new("data/xyz/water90.xyz", 0);
+    exec_times.stop("molecule");
+
     println!("Molecule: {:?}", mol);
+    exec_times.stop("total");
+
+    //##################################
+    //###           FOOTER           ###
+    //##################################
+    exec_times.print();
 }
--- src/print_utils/mod.rs
+++ src/print_utils/mod.rs
@@ -1,3 +1,95 @@
-// pub fn print_header() {
-//     
+use std::{collections::HashMap, time::Instant};
+
+pub fn print_prog_header() {
+    //     const HEADER_V1: &str = r#"
+    // ___  ___      _   _____ _            ______  _____
+    // |  \/  |     | | |  ___| |           | ___ \/  ___|
+    // | .  . | ___ | | | |__ | | ___  ___  | |_/ /\ `--.
+    // | |\/| |/ _ \| | |  __|| |/ _ \/ __| |    /  `--. \
+    // | |  | | (_) | | | |___| |  __/ (__  | |\ \ /\__/ /
+    // \_|  |_/\___/|_| \____/|_|\___|\___| \_| \_|\____/
+    // "#;
+    //
+    //     const HEADER_V2: &str = r#"
+    //      __  ___      __   ________          ____  _____
+    //     /  |/  /___  / /  / ____/ /__  _____/ __ \/ ___/
+    //    / /|_/ / __ \/ /  / __/ / / _ \/ ___/ /_/ /\__ \
+    //   / /  / / /_/ / /  / /___/ /  __/ /__/ _, _/___/ /
+    //  /_/  /_/\____/_/  /_____/_/\___/\___/_/ |_|/____/
+    // "#;
+    //
+    //     const HEADER_V3: &str = r#"
+    //      __  ___      __            ________          ____  _____
+    //     /  |/  /___  / /           / ____/ /__  _____/ __ \/ ___/
+    //    / /|_/ / __ \/ /  ______   / __/ / / _ \/ ___/ /_/ /\__ \
+    //   / /  / / /_/ / /  /_____/  / /___/ /  __/ /__/ _, _/___/ /
+    //  /_/  /_/\____/_/           /_____/_/\___/\___/_/ |_|/____/
+    //     "#;
+    //
+    //     const HEADER_V4: &str = r#"
+    //     888b     d888          888        8888888888 888                   8888888b.   .d8888b.
+    //     8888b   d8888          888        888        888                   888   Y88b d88P  Y88b
+    //     88888b.d88888          888        888        888                   888    888 Y88b.
+    //     888Y88888P888  .d88b.  888        8888888    888  .d88b.   .d8888b 888   d88P  "Y888b.
+    //     888 Y888P 888 d88""88b 888 888888 888        888 d8P  Y8b d88P"    8888888P"      "Y88b.
+    //     888  Y8P  888 888  888 888        888        888 88888888 888      888 T88b         "888
+    //     888   "   888 Y88..88P 888        888        888 Y8b.     Y88b.    888  T88b  Y88b  d88P
+    //     888       888  "Y88P"  888        8888888888 888  "Y8888   "Y8888P 888   T88b  "Y8888P"
+    //     "#;
+    //
+    // println!("{}", HEADER_V1);
+    // println!("{}", HEADER_V2);
+    // println!("{}", HEADER_V3);
+    // println!("{}", HEADER_V4);
+
+    const HEADER_V5: &str = r#"
+  ,ggg, ,ggg,_,ggg,                                   ,ggggggg,                          ,ggggggggggg,         ,gg,   
+ dP""Y8dP""Y88P""Y8b               ,dPYb,           ,dP""""""Y8b ,dPYb,                 dP"""88""""""Y8,      i8""8i  
+ Yb, `88'  `88'  `88               IP'`Yb           d8'    a  Y8 IP'`Yb                 Yb,  88      `8b      `8,,8'  
+  `"  88    88    88               I8  8I           88     "Y8P' I8  8I                  `"  88      ,8P       `88'   
+      88    88    88               I8  8'           `8baaaa      I8  8'                      88aaaad8P"        dP"8,  
+      88    88    88    ,ggggg,    I8 dP  aaaaaaaa ,d8P""""      I8 dP   ,ggg,     ,gggg,    88""""Yb,        dP' `8a 
+      88    88    88   dP"  "Y8ggg I8dP   """""""" d8"           I8dP   i8" "8i   dP"  "Yb   88     "8b      dP'   `Yb
+      88    88    88  i8'    ,8I   I8P             Y8,           I8P    I8, ,8I  i8'         88      `8i _ ,dP'     I8
+      88    88    Y8,,d8,   ,d8'  ,d8b,_           `Yba,,_____, ,d8b,_  `YbadP' ,d8,_    _   88       Yb,"888,,____,dP
+      88    88    `Y8P"Y8888P"    8P'"Y88            `"Y8888888 8P'"Y88888P"Y888P""Y8888PP   88        Y8a8P"Y88888P" 
+"#;
+
+    println!("{}", HEADER_V5);
+}
+
+// struct Timer {
+//     timings: Vec<Timinig>,
 // }
+
+pub struct ExecTimes {
+    timings_map: HashMap<String, [Instant; 2]>,
+}
+
+impl ExecTimes {
+    pub fn new() -> Self {
+        Self {
+            timings_map: HashMap::new(),
+        }
+    }
+
+    pub fn start(&mut self, name: &str) {
+        let now = Instant::now();
+        self.timings_map.insert(name.to_string(), [now, now]);
+    }
+
+    pub fn stop(&mut self, name: &str) {
+        let now = Instant::now();
+        if let Some(timings) = self.timings_map.get_mut(name) {
+            timings[1] = now;
+        }
+    }
+
+    pub fn print(&self) {
+        println!("Execution times:");
+        for (name, timings) in &self.timings_map {
+            let exec_time = timings[1].duration_since(timings[0]);
+            println!("  {}: {:?}", name, exec_time);
+        }
+    }
+}
