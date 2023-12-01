17594ed781326a82af392141cb73ad8bb909c18a
MRJD <M.Dagleish@campus.lmu.de>
2023-12-01 23:17:47 +0100

    Add calc_type module

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod basisset;
 mod mol_int_and_deriv;
 mod molecule;
 mod print_utils;
+mod calc_type;
 
 use crate::print_utils::print_prog_header;
 use molecule::Molecule;
@@ -16,16 +17,30 @@ fn main() {
     //###           HEADER           ###
     //##################################
     let mut exec_times = print_utils::ExecTimes::new();
-    exec_times.start("total");
+    exec_times.start("Total");
 
     print_prog_header();
 
-    exec_times.start("molecule");
+    exec_times.start("Molecule");
     let mol = Molecule::new("data/xyz/water90.xyz", 0);
-    exec_times.stop("molecule");
+    // println!("Molecule: {:?}", mol);
+    exec_times.stop("Molecule");
 
-    println!("Molecule: {:?}", mol);
-    exec_times.stop("total");
+    exec_times.start("BasisSet");
+    // let mut basisset = basisset::BasisSet::new("data/basisset/sto-3g.json");
+    
+    exec_times.stop("BasisSet");
+
+    //##################################
+    //###           BODY             ###
+    //##################################
+    // Calculation type
+    // let calc_type = "";
+    
+
+
+
+    exec_times.stop("Total");
 
     //##################################
     //###           FOOTER           ###
