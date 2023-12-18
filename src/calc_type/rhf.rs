cf92aa64b9a0d848748dfc5b2a9f9a5a4570426a
MRJD <M.Dagleish@campus.lmu.de>
2023-12-18 21:36:43 +0100

    Add potential energy matrix calculation to calc_1e_int_matrs function

--- src/calc_type/rhf.rs
+++ src/calc_type/rhf.rs
@@ -1,14 +1,15 @@
 use crate::basisset::BasisSet;
-use crate::mol_int_and_deriv::oe_int::{calc_kinetic_int_cgto, calc_overlap_int_cgto};
+use crate::mol_int_and_deriv::oe_int::{
+    calc_kinetic_int_cgto, calc_overlap_int_cgto, calc_pot_int_cgto,
+};
+use crate::molecule::Molecule;
 use ndarray::Array2;
 
-pub fn calc_1e_int_matrs(basis: &BasisSet) -> (Array2<f64>, Array2<f64>) {
+pub fn calc_1e_int_matrs(basis: &BasisSet, mol: &Molecule) -> (Array2<f64>, Array2<f64>, Array2<f64>) {
     let mut S_matr = Array2::<f64>::zeros((basis.no_bf(), basis.no_bf()));
     let mut T_matr = Array2::<f64>::zeros((basis.no_bf(), basis.no_bf()));
+    let mut V_matr = Array2::<f64>::zeros((basis.no_bf(), basis.no_bf()));
 
-    /////////////////////////////////
-    // 2nd version
-    /////////////////////////////////
     let mut mu_sh_offset: usize = 0;
     let mut nu_sh_offset: usize = 0;
     let no_shells = basis.no_shells();
@@ -20,22 +21,22 @@ pub fn calc_1e_int_matrs(basis: &BasisSet) -> (Array2<f64>, Array2<f64>) {
                 let mu_idx = mu_sh_offset + cgto_idx1;
                 for (cgto_idx2, cgto2) in shell2.cgto_iter().enumerate() {
                     let nu_idx = nu_sh_offset + cgto_idx2;
-                    if mu_idx == nu_idx {
-                        S_matr[(mu_idx, nu_idx)] = 1.0;
-                        T_matr[(mu_idx, nu_idx)] = calc_kinetic_int_cgto(cgto1, cgto2);
-                        continue;
+                    S_matr[(mu_idx, nu_idx)] = if mu_idx == nu_idx {
+                        1.0
                     } else {
-                        S_matr[(mu_idx, nu_idx)] = calc_overlap_int_cgto(cgto1, cgto2);
-                        S_matr[(nu_idx, mu_idx)] = S_matr[(mu_idx, nu_idx)];
-                        T_matr[(mu_idx, nu_idx)] = calc_kinetic_int_cgto(cgto1, cgto2);
-                        T_matr[(nu_idx, mu_idx)] = T_matr[(mu_idx, nu_idx)];
-                    }
+                        calc_overlap_int_cgto(cgto1, cgto2)
+                    };
+                    S_matr[(nu_idx, mu_idx)] = S_matr[(mu_idx, nu_idx)];
+                    T_matr[(mu_idx, nu_idx)] = calc_kinetic_int_cgto(cgto1, cgto2);
+                    T_matr[(nu_idx, mu_idx)] = T_matr[(mu_idx, nu_idx)];
+                    V_matr[(mu_idx, nu_idx)] = calc_pot_int_cgto(cgto1, cgto2, mol);
+                    V_matr[(nu_idx, mu_idx)] = V_matr[(mu_idx, nu_idx)];
                 }
             }
-            nu_sh_offset += shell2.shell_len();
+            nu_sh_offset += basis.shell_len(sh_idx2);
         }
         nu_sh_offset = 0;
-        mu_sh_offset += shell1.shell_len();
+        mu_sh_offset += basis.shell_len(sh_idx1);
     }
     //
 
@@ -64,7 +65,7 @@ pub fn calc_1e_int_matrs(basis: &BasisSet) -> (Array2<f64>, Array2<f64>) {
     //     mu_sh_offset += shell1.shell_len();
     // }
 
-    (S_matr, T_matr)
+    (S_matr, T_matr, V_matr)
 }
 
 #[cfg(test)]
@@ -78,8 +79,9 @@ mod tests {
         let mol = Molecule::new("data/xyz/water90.xyz", 0);
         let basis = BasisSet::new("STO-3G", &mol);
 
-        let (S_matr, T_matr) = calc_1e_int_matrs(&basis);
+        let (S_matr, T_matr, V_matr) = calc_1e_int_matrs(&basis, &mol);
         println!("{}", S_matr);
         println!("{}", T_matr);
+        println!("{}", V_matr);
     }
 }
